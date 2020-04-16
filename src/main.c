f5fc6a6bba1805e7a5e7b4b90c80f0c8dfa828da
anthony.denin@epitech.eu <anthony.denin@epitech.eu>
2020-04-16 12:36:29 +0000

    [ADD]main -> error ctrl + c

--- src/main.c
+++ src/main.c
@@ -7,6 +7,13 @@
 
 #include "my.h"
 
+void msg_ctrl(int test)
+{
+    (void)test;
+    my_putchar('\n');
+    put_prompt();
+}
+
 int main(__attribute__((unused)) int ac,
     __attribute__((unused)) char **av, char **env)
 {
@@ -16,6 +23,7 @@ int main(__attribute__((unused)) int ac,
 
     if (put_prompt())
         return EPITECH_ERROR;
+    signal(SIGINT, msg_ctrl);
     for (size_t BUFFER_SIZE = 0; getline(&buffer, &BUFFER_SIZE, stdin) != -1;
         BUFFER_SIZE = 0) {
         quit = do_loop(buffer, &env);
